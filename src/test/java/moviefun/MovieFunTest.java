8829ada5f58d6c61956302633ad991afebe69986
Damien Le Berrigaud <damien@itsdamo.com>
2016-12-19 13:42:25 -0700

    New smoke test does not depend on Spring.

--- src/test/java/moviefun/MovieFunTest.java
+++ src/test/java/moviefun/MovieFunTest.java
@@ -1,51 +1,44 @@
 package moviefun;
 
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.superbiz.moviefun.Application;
+import org.springframework.web.client.RestTemplate;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class, webEnvironment = RANDOM_PORT)
 public class MovieFunTest {
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    @Before
-    public void setup() {
-        jdbcTemplate.execute("DELETE FROM movie");
-    }
 
     @Test
     public void smokeTest() {
-        String homePage = restTemplate.getForObject("/", String.class);
+        RestTemplate restTemplate = new RestTemplate();
 
-        assertThat(homePage).contains("Please select one of the following links:");
+        String homePage = restTemplate.getForObject(url("/"), String.class);
 
-        String setupPage = restTemplate.getForObject("/setup", String.class);
+        assertThat(homePage, containsString("Please select one of the following links:"));
 
-        assertThat(setupPage).contains("Wedding Crashers");
-        assertThat(setupPage).contains("Starsky & Hutch");
-        assertThat(setupPage).contains("Shanghai Knights");
-        assertThat(setupPage).contains("I-Spy");
-        assertThat(setupPage).contains("The Royal Tenenbaums");
+        String setupPage = restTemplate.getForObject(url("/setup"), String.class);
 
-        String movieFunPage = restTemplate.getForObject("/moviefun", String.class);
+        assertThat(setupPage, containsString("Wedding Crashers"));
+        assertThat(setupPage, containsString("Starsky & Hutch"));
+        assertThat(setupPage, containsString("Shanghai Knights"));
+        assertThat(setupPage, containsString("I-Spy"));
+        assertThat(setupPage, containsString("The Royal Tenenbaums"));
 
-        assertThat(movieFunPage).contains("Wedding Crashers");
-        assertThat(movieFunPage).contains("David Dobkin");
+        String movieFunPage = restTemplate.getForObject(url("/moviefun"), String.class);
+
+        assertThat(movieFunPage, containsString("Wedding Crashers"));
+        assertThat(movieFunPage, containsString("David Dobkin"));
+    }
+
+    private String url(String path) {
+        String baseUrl = "http://localhost:8080";
+        String envUrl = System.getenv("MOVIE_FUN_URL");
+
+        if (envUrl != null && !envUrl.isEmpty()) {
+            baseUrl = envUrl;
+        }
+
+        return baseUrl + path;
     }
 }
