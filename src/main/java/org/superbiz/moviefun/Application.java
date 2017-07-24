08a54c3ea55a726837b964527f656f5f3afb2504
Dev Floater 17 <pivotal@Dev-Floater-17.local>
2017-07-24 12:12:55 -0400

    spring bootification

---
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -0,0 +1,21 @@
+package org.superbiz.moviefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Created by pivotal on 7/24/17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(ActionServlet actionServlet) {
+        return new ServletRegistrationBean(actionServlet, "/moviefun/*");
+    }
+}
--- src/main/java/org/superbiz/moviefun/MoviesBean.java
+++ src/main/java/org/superbiz/moviefun/MoviesBean.java
@@ -16,24 +16,28 @@
  */
 package org.superbiz.moviefun;
 
+import org.springframework.stereotype.Repository;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.*;
 import javax.persistence.metamodel.EntityType;
+import javax.transaction.Transactional;
 import java.util.List;
 
-@Stateless
+@Repository
 public class MoviesBean {
 
-    @PersistenceContext(unitName = "movie-unit")
+    @PersistenceContext
     private EntityManager entityManager;
 
     public Movie find(Long id) {
         return entityManager.find(Movie.class, id);
     }
 
+    @Transactional
     public void addMovie(Movie movie) {
         entityManager.persist(movie);
     }
